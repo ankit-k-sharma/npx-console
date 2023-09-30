8f5ae3ea216c9c0e4eaa1e29f3122611db18d0cc
Ankit Kumar Sharma <aks.computerexpert@gmail.com>
2023-10-01 03:04:41 +0530

    Add npx console part

---
+++ index.js
@@ -0,0 +1,8 @@
+#! /usr/bin/env node
+console.log("Hello, Ankit!!! ....");
+console.log('\x1B[31mAnkit Kumar Sharma ');
+console.log('\x1B[37m----------------------')
+console.log('\x1B[31m       ████          █        \n      ██  ██\n     ██    ██\n    ██      ██\n   ██        ██\n  ██          ██');
+console.log('\x1B[37m----------------------');
+
+// For run on local, you have to use >> npm i -g and then npx ankit-info 
