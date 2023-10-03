dbab1d374456f207b7e4ef4318b980fb2c1ec622
Ankit Kumar Sharma <aks.computerexpert@gmail.com>
2023-10-04 00:50:37 +0530

    Add styled name format

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 #! /usr/bin/env node
-console.log("Hello, Ankit!!! ....");
-console.log('\x1B[31mAnkit Kumar Sharma ');
-console.log('\x1B[37m----------------------')
-console.log('\x1B[31m       ████          █        \n      ██  ██\n     ██    ██\n    ██      ██\n   ██        ██\n  ██          ██');
-console.log('\x1B[37m----------------------');
-
-// For run on local, you have to use >> npm i -g and then npx ankit-info 
+console.log('\x1B[37m=========================================================================');
+console.log('\x1B[31mWelcome Buddy - About me !!! ');
+console.log('\x1B[37m-------------------------------------------------------------------------');
+console.log('\x1B[31m       ████          ████     ██     ██    ██     ██     ██████████\n      ██  ██         ██ ██    ██     ██   ██      ██         ██\n     ██    ██        ██  ██   ██     ██ ███       ██         ██\n    ██  **  ██       ██   ██  ██     ██ ███       ██         ██\n   ██        ██      ██    ██ ██     ██   ██      ██         ██\n  ██          ██     ██     ████     ██    ██     ██         ██');
+console.log('\x1B[37m-------------------------------------------------------------------------');
+// For run on local, you have to use >> npm i -g and then npx @ankit/about-me 
